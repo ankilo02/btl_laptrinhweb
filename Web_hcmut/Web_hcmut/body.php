234a3846a3e3b8e36ca093934b772a25d016bcbe
quy2808 <quylun2808@gmail.com>
2023-04-25 18:19:23 +0700

    web

---
+++ Web_hcmut/Web_hcmut/body.php
@@ -0,0 +1,180 @@
+
+    <div class="pic">
+        <div class="group4"></div>
+                <div class="group5"></div>
+                <div class="group6"></div>
+                <div class="group7"></div>
+                <div class="group8"></div>
+                <div class="group9"></div>
+    </div>
+    <div class="mainnn">
+            <p class="elza">BK Coffee</p>
+            <p class="text1">Hôm nay là một ngày tuyệt vời để tìm kiếm cốc cà phê của chính bạn</p>
+            <div class="button1">
+                <p><a class="text2" href="./menu.php">Mua ngay</a></p>
+            </div>
+            <div class="button2">
+                <p><a class="text3" href="./index.php">Catalog</a></p>
+            </div>
+            <div class="card">
+                <div class="cup1"></div>
+                <p class="text5">Cốc cà phê đầu tiên</p>
+                <div class="frame8">
+                    <a class="text4" href="./menu.php">Hurry Up</a>
+                    <img src="img/giohang.png" width="21.64" height="21.64">
+                </div>
+            </div>
+    </div>
+    <div id="section2">
+            <div class="story">
+                <p class="text6">Giới thiệu</p>
+                <div class="sframe">
+                    <div class="group1">
+                        <img src="img/Group1.png">
+                    </div>
+                    <div class="group2">
+                        <img src="img/Group2.png">
+                    </div>
+                    <div class="group3">
+                        <img src="img/Group3.png">
+                    </div>
+                    <p class="text7">BK Coffee là một cửa hàng cà phê trực tuyến cung cấp nhiều lựa chọn về cà phê đặc sản từ khắp nơi trên thế giới. Từ nguồn gốc đơn lẻ rang đậm vừa phải đến hạt cà phê espresso có hương vị, chúng tôi cung cấp nhiều loại sản phẩm có nguồn gốc hợp pháp để kích thích khẩu vị của bất kỳ khách hàng nào. Đối với những người đang tìm kiếm thiết bị pha cà phê độc đáo, Bk Coffee cũng cung cấp đầy đủ các loại máy pha: máy pha cà phê espresso,...</p>
+                    <a class="text8">Thêm</a>
+                </div>
+                <div class="bframe">
+                    <div class="face">
+
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div id="section3">
+            <div class="service">
+                
+            </div>
+            <p class="text9">Dịch vụ</p>
+                <div class="frame1">
+                    <div class="e1">
+                        <div class="icon1"></div>
+                        <a class="text10" href="./menu.php">Thiết bị</a>
+                    </div>
+                    <div class="e2">
+                        <div class="icon2"></div>
+                        <a class="text11" href="./menu.php">Loại cà phê</a>
+                    </div>
+                    <div class="e3">
+                        <div class="icon3"></div>
+                        <a class="text12" href="./menu.php">Mang đi</a>
+                    </div>
+                    <div class="e4">
+                        <div class="icon4"></div>
+                        <a class="text13" href="./menu.php">Loại hạt</a>
+                    </div>
+                    <div class="e5">
+                        <div class="icon5"></div>
+                        <a class="text14" href="./menu.php">Bánh ngọt</a>
+                    </div>
+                </div>
+    </div>
+    <div class="section4">
+            <div class="offer">
+                <div class="offrame">
+                    <div class="frame2">
+                        <a href="./menu.php" class="text16">Mua ngay</a>
+                    </div>
+                    <p class="text17">lên tới 50%</p>
+                    <p class="text18">Tại quán cà phê của chúng tôi, chúng tôi tự hào cung cấp cho khách hàng loại cà phê ngon nhất xung quanh. Cà phê được lựa chọn cẩn thận của chúng tôi đến từ một số vùng trồng cà phê nổi tiếng nhất trên thế giới, đảm bảo rằng mỗi tách cà phê đều có hương vị và độ tươi ngon độc nhất</p>
+                    <div class="pack"></div>
+                    <div class="elip1"></div>
+                    <div class="elip2"></div>
+                </div>
+                <p class="text15">Giảmgiá</p>
+            </div>
+    </div>
+    <div class="section5">
+            <p class="text20">Popular menu</p>
+            <div class="menu1">
+                <a class="text19" href="./menu.php">Menu</a>
+            </div>
+            <div class="menu2">
+                <div class="m1">
+                    <div class="group10">
+                        <p class="text21">arabica</p>
+                        <div class="vector4"></div>
+                    </div>
+                </div>
+                <div class="m2">
+                    <div class="group11">
+                    <p class="text21">Robusta</p>
+                    <div class="vector5"></div>
+                    </div>
+                </div>
+                <div class="m3">
+                    <div class="group10">
+                        <p class="text21">Excelsa</p>
+                        <div class="vector4"></div>
+                    </div>
+                </div>
+                <div class="m4">
+                    <div class="group11">
+                    <p class="text21">Liberica</p>
+                    <div class="vector5"></div>
+                    </div>
+                </div>
+                <div class="m5">
+                    <div class="group10">
+                        <p class="text21">Chocolate</p>
+                        <div class="vector4"></div>
+                    </div>
+                </div>
+                <div class="m6">
+                    <div class="group11">
+                    <p class="text21">maragogype</p>
+                    <div class="vector5"></div>
+                    </div>
+                </div>
+                <div class="m7">
+                    <div class="group10">
+                        <p class="text21">Moka</p>
+                        <div class="vector4"></div>
+                    </div>
+                </div>
+                <div class="m8">
+                    <div class="group11">
+                    <p class="text21">Tupica</p>
+                    <div class="vector5"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="glow3"></div>
+            <div class="poimg"></div>
+            
+    </div>
+    <div id="section6">
+        
+        <div class="blog">
+            <div class="blogs">
+                <div class="blog1">
+                    <a class="text24" href="#">Thêm</a>
+                    <p class="text25">Pha một tách cà phê không hề phức tạp. Bắt đầu bằng cách thu thập nguồn cung cấp của bạn: bã cà phê, nước và bình ép kiểu Pháp, cốc hoặc bình khác. Tiếp theo, đong 2 thìa cà phê xay trên 8 ounce nước. Nếu bạn đang sử dụng máy ép kiểu Pháp, hãy đổ lượng bã đã đo ...</p>
+                    <p class="text26">Cách làm tách cà phê hoàn hảo </p>
+                    <div class="frame3"></div>
+                    <div class="frame4"></div>
+                </div>
+                <div class="blog2">
+                    <a class="text24" href="#">Thêm</a>
+                    <p class="text25">Caffeine hoạt động bằng cách ngăn chặn các thụ thể adenosine trong não, giúp tạo điều kiện sản xuất năng lượng và khiến chúng ta cảm thấy tỉnh táo hơn. Caffeine cũng làm tăng nồng độ adrenaline, tăng thêm năng lượng và nhịp tim, cũng như các hormone khác như dopamine có thể khiến bạn tỉnh táo hơn. Sự kết hợp của những ...</p>
+                    <p class="text26">Tại sao cà phê làm chúng ta tỉnh táo?</p>
+                    <div class="frame5"></div>
+                </div>
+            </div>
+            <div class="arow">
+                <a href="#" class="avec">
+                    <h1 class="acolor"><i class="fa fa-chevron-right" aria-hidden="true"></i></h1>
+                </a>
+            </div>
+        </div>
+        <p class="text23">Blog</p>
+    </div>
+    <div class="v">
+    </div>
---
+++ Web_hcmut/Web_hcmut/footer.php
@@ -0,0 +1,82 @@
+<footer  id="footer">
+    <!-- top footer -->
+    <div class="section">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="footer">
+                        <img src="img/cafe.png" width=323px height=383px></img>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-8 col-xs-12 text-left" style="margin-top:0px;">
+                    <div class="footer">
+                    <h3 class="footer-title">Các loại đồ uống</h3>
+                        <ul class="footer-links">
+                            <?php
+                            include 'db.php';
+                            $result = mysqli_query($con, 'SELECT * FROM categories');
+                            foreach ($result as $cat) {
+                                $cat_id = $cat['cat_id'];
+                                $cat_title = $cat['cat_title'];
+                                echo "<li><a href='store.php?cat=$cat_id'>$cat_title</a></li>";
+                            }
+                            ?>
+                        </ul>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+                    <div class="footer">
+                        <h3 class="footer-title">Về chúng tôi</h3>
+                        <p class="linkss"> Công Ty Cổ Phần - BK Coffee -15-17 Tôn Đức Thắng, Quận 1
+                            , TP. HCM, Việt Nam, nhận đặt hàng trực tuyến và giao hàng tận
+                            nơi.
+                            <br>
+                            KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn phòng cũng như tất cả Hệ Thống trên toàn
+                            quốc.
+                        </p>
+                        <ul class="footer-links">
+                            <li><a href="#" class="linkss"><i class="fa fa-map-marker"></i>15-17 Tôn Đức Thắng, Q.1, TP. HCM</a></li>
+                            <li><a href="#" class="linkss"><i class="fa fa-phone"></i>+84-0123456789</a></li>
+                            <li><a href="#" class="linkss"><i class="fa fa-envelope-o"></i>bk@hcmut.edu.vn</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer">
+                        <h3 class="footer-title">Liên hệ </h3>
+                        <div class="social">
+                            <a href="#"><img src="assets/images/facebook.png" alt="facebook" /></a>
+                            <a href="#"><img src="assets/images/twitter.png" alt="twitter" /></a>
+                            <a href="#"><img src="assets/images/instagram.png" alt="instagram" /></a>
+                            <a href="#"><img src="assets/images/youtube.png" alt="youtube" /></a>
+                            <a href="#"><img src="assets/images/telegram.png" alt="telegram" /></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix visible-xs"></div>
+
+
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /top footer -->
+
+    <div id="go-to-top">
+        <i class='fa fa-caret-up'></i>
+    </div>
+
+</footer>
+<script src="vendor/js/jquery.min.js"></script>
+<script src="vendor/js/bootstrap.min.js"></script>
+<script src="vendor/js/slick.min.js"></script>
+<script src="assets/js/actions.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+
+</body>
+
+</html>
